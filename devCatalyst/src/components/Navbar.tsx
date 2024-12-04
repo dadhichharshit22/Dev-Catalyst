c6f251fabffcc10cd162ee0c3c65225eacff405b
dadhichharshit22 <dadhich.harshit22122002@gmail.com>
2024-12-05 00:59:08 +0530

    Initial Commit

---
+++ devCatalyst/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src='./assets/react.svg' alt="Dev Catalyst Logo"/>
+      </div>
+      <div>
+        <p>Courses</p>
+        <p>About</p>
+      </div>
+      <div>
+        <img src="" alt="Dark Mode"/>
+        <button>Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
